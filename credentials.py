84e88bb49507c69c8756c916f33e75d8e3e0f193
rohaan malik <rohaanmalik7@gmail.com>
2021-05-10 23:19:42 +0530

    added cred

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
-bot_token = "here goes your access token from BotFather"
-bot_user_name = "the username you entered"
+bot_token = "1894370388:AAHh6CmbHf8P2y0nkLXOhf63wL5yUlZhwMc"
+bot_user_name = "VaxHunterNagpur_bot"
 URL = "the heroku app link that we will create later"
